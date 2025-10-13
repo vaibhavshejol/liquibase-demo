7d740a5dc5ba7b28dc9644dc2bc9be43d17ab004
Vaibhav Shejol <vaibhav.shejol@bnt-soft.com>
2025-10-13 16:06:57 +0530

    using .sql file

---
+++ src/main/resources/db/changelog/changes/DML-1.0.0.1.sql
@@ -0,0 +1,2 @@
+INSERT INTO skill (id, primary_skill, secondary_skill, skills, employee_id) VALUES (1, 'Java', 'Spring Boot', 'data/John-Doe-skills.json', 1);
+INSERT INTO skill (id, primary_skill, secondary_skill, skills, employee_id) VALUES (2, 'JUnit' 'Test Cases', 'data/Jane-Smith-skills.json', 2);
