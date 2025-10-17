33457260b0fdacc65c35a691782827714934f09a
Vaibhav Shejol <vaibhav.shejol@bnt-soft.com>
2025-10-17 16:34:55 +0530

    change encoding

--- src/main/java/com/bnt/liquibasedemo/LiquibaseDemoApplication.java
+++ src/main/java/com/bnt/liquibasedemo/LiquibaseDemoApplication.java
@@ -1,5 +1,6 @@
 package com.bnt.liquibasedemo;
 
+import org.apache.commons.lang3.SystemProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LiquibaseDemoApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("file.encoding", "UTF-8");
+		java.nio.charset.Charset.defaultCharset();
 		SpringApplication.run(LiquibaseDemoApplication.class, args);
+		System.out.println("Default charset: " + java.nio.charset.Charset.defaultCharset());
+		System.out.println("Default file en: " + SystemProperties.getProperty("file.encoding"));
 	}
 
 }
