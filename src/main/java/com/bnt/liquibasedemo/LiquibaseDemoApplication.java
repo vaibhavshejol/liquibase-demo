702bf49607a751515f0b5e7314107a9bac9ed350
Vaibhav Shejol <vaibhav.shejol@bnt-soft.com>
2025-10-28 15:40:40 +0530

    worked using type change and type normalize

--- src/main/java/com/bnt/liquibasedemo/LiquibaseDemoApplication.java
+++ src/main/java/com/bnt/liquibasedemo/LiquibaseDemoApplication.java
@@ -8,11 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LiquibaseDemoApplication {
 
 	public static void main(String[] args) {
-//		System.setProperty("file.encoding", "UTF-8");
-//		java.nio.charset.Charset.defaultCharset();
 		SpringApplication.run(LiquibaseDemoApplication.class, args);
-		System.out.println("Default charset: " + java.nio.charset.Charset.defaultCharset());
-		System.out.println("Default file en: " + SystemProperties.getProperty("file.encoding"));
 	}
 
 }
